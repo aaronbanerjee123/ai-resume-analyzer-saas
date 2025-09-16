3cf240a522fc96b08d0eb7af2088cb92aed3724a
aaronbanerjee123 <aaronbaner@outlook.com>
2025-09-15 23:56:27 -0700

    implemented auth foundation with puter, log in and sign up works with redirects

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,21 +1,59 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
-export const meta = () => (
-    [{title:'Resumind | Auth'},
-        {name:'description', content: 'Log into your account'},
-    ])
+export const meta = () => [
+  { title: "Resumind | Auth" },
+  { name: "description", content: "Log into your account" },
+];
 
 const Auth = () => {
+  const { isLoading, auth } = usePuterStore();
+  const location = useLocation();
+
+  const searchParams = new URLSearchParams(location.search);
+  const next = searchParams.get("next") || "/"; // Default to home page
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth.isAuthenticated) {
+      setTimeout(() => {
+        navigate(next, { replace: true });
+      }, 100);
+    }
+  }, [auth.isAuthenticated, next,navigate]);
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-cover min-h-screen flex items-center justify-center ">
-        <div className="gradient-border shadow-lg">
-            <section className="flex flex-col gap-8 bg-white rounded-2xl px-10">
-                <h1>Welcome</h1>
-                <h2>Log In to Continue your Job Journey</h2>
-            </section>
-        </div>
+      <div className="gradient-border shadow-lg">
+        <section className="flex flex-col gap-8 bg-white rounded-2xl px-10">
+          <div className="flex flex-col items-center justify-center gap-2 text-center">
+            <h1>Welcome</h1>
+            <h2>Log In to Continue your Job Journey</h2>
+          </div>
+          <div>
+            {isLoading ? (
+              <button className="auth-button animate-pulse">
+                <p>Signing you in ...</p>
+              </button>
+            ) : (
+              <>
+                {auth.isAuthenticated ? (
+                  <button className="auth-button" onClick={auth.signOut}>
+                    <p>Log out</p>
+                  </button>
+                ) : (
+                  <button className="auth-button" onClick={auth.signIn}>
+                    <p>Sign in</p>
+                  </button>
+                )}
+              </>
+            )}
+          </div>
+        </section>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Auth
+export default Auth;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,6 +3,9 @@ import { Welcome } from "../welcome/welcome";
 import Navbar from "~/components/navbar";
 import { resumes } from "/constants";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useLocation, useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,6 +15,15 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const {isLoading, auth} = usePuterStore();
+    const location = useLocation();
+
+    const navigate = useNavigate();
+    useEffect(() => {
+        if(!auth.isAuthenticated){
+            navigate('/auth?next=/');
+        }
+    },[auth.isAuthenticated])
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
